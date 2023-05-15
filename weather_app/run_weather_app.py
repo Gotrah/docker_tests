849dc506cb97bd9f27314c13e108aa344a87ef0d
Alex <hartog.alex@gmail.com>
2023-05-15 13:47:39 +0200

    Updated docker

--- weather_app/run_weather_app.py
+++ weather_app/run_weather_app.py
@@ -6,9 +6,9 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__name__))
-
 from weather_app import create_app
+
+sys.path.append(os.path.dirname(__name__))
 
 # create an app instance
 app = create_app()
